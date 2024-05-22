f2aaf77e2ca5ef364c416e43187c4d7c5cf4816b
elizabethhe135 <elizahe@seas.upenn.edu>
2024-05-21 23:50:00 -0400

    modify

--- inference.py
+++ inference.py
@@ -2,8 +2,8 @@ import os.path
 import pdb
 
 import torch
-from diffusers import UniPCMultistepScheduler, AutoencoderKL
-from diffusers.pipelines import StableDiffusionPipeline
+from diffusers import UniPCMultistepScheduler, AutoencoderKL, ControlNetModel
+from diffusers.pipelines import StableDiffusionControlNetPipeline
 from PIL import Image
 import argparse
 
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     parser.add_argument('--enable_cloth_guidance', action="store_true")
     parser.add_argument('--pipe_path', type=str, default="SG161222/Realistic_Vision_V4.0_noVAE")
     parser.add_argument('--output_path', type=str, default="./output_img")
+    parser.add_argument('--person_path', type=str, required=False)
 
     args = parser.parse_args()
 
@@ -27,15 +28,16 @@ if __name__ == "__main__":
         os.makedirs(output_path)
 
     cloth_image = Image.open(args.cloth_path).convert("RGB")
+    control_net_openpose = ControlNetModel.from_pretrained("lllyasviel/control_v11p_sd15_inpaint", torch_dtype=torch.float16)
 
     vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse").to(dtype=torch.float16)
     if args.enable_cloth_guidance:
-        pipe = OmsDiffusionPipeline.from_pretrained(args.pipe_path, vae=vae, torch_dtype=torch.float16)
+        pipe = OmsDiffusionControlNetPipeline.from_pretrained(args.pipe_path, vae=vae, controlnet=control_net_openpose, torch_dtype=torch.float16)
     else:
-        pipe = StableDiffusionPipeline.from_pretrained(args.pipe_path, vae=vae, torch_dtype=torch.float16)
+        pipe = StableDiffusionControlNetPipeline.from_pretrained(args.pipe_path, vae=vae, controlnet=control_net_openpose, torch_dtype=torch.float16)
     pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 
-    full_net = ClothAdapter(pipe, args.model_path, device, args.enable_cloth_guidance, False)
+    full_net = ClothAdapter(pipe, args.model_path, device, args.enable_cloth_guidance, False, person_path=args.person_path)
     images = full_net.generate(cloth_image)
     for i, image in enumerate(images[0]):
         image.save(os.path.join(output_path, "out_" + str(i) + ".png"))
